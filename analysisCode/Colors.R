0d50cfb43d633e92bb604d331572a6cc4b54938f
matthiaswietz <45360784+matthiaswietz@users.noreply.github.com>
2021-11-26 11:10:40 +0100

    Add files via upload

---
+++ analysisCode/Colors.R
@@ -0,0 +1,132 @@
+
+col.taxon <- c(
+  "Bacteroidia"= "#5fd35f", 
+  "Flavobacteriales" = "#5fd35f", 
+  "Flavobacteriaceae" = "#5fd35f",
+  "Aurantivirga" = "#37abc8", 
+  "Gammaproteobacteria uc" = "#2ad4ff",
+  "Colwellia" = "#a9ccae",
+  
+  "Dadabacteriales" = "#6b706e", 
+  "Dadabacteriales uc" = "#6b706e", 
+ 
+  "Nitrososphaeria" = "#ffe72e", 
+  "Nitrosopumilales" = "#ffe72e", 
+  "Nitrosopumilaceae" = "#ffe72e",
+
+  "Alphaproteobacteria" ="#4769d1",
+  "Rhodobacterales" = "#85847b", 
+  "Rhodobacteraceae" = "#85847b",
+ 
+  "Planctomycetes" = "#ffae1a", 
+  "Planctomycetales" = "#ffae1a",
+  "Planctomycetacia"="#ffae1a", 
+  
+  "Verrucomicrobiae"= "#d40000", 
+  "Verrucomicrobiales" = "#d40000",
+  "Arctic97B-4" = "#d40000",
+  "Arctic97B-4 uc" = "#d40000",
+  "Marinimicrobia_SAR406 uc"= "#88f6e1", 
+
+
+  ################  EUK  ################ 
+  
+  "Bacillariophyta" = "#614257", 
+  "Raphid-pennate" = "#ff8c00",
+  "Araphid-pennate" ="#57053d",
+  "Mediophyceae" = "#ed51e0",
+  "Gyrodinium" = "#dbd9b6",
+  "Pseudo-nitzschia" = "#82467f",
+  "Chaetoceros" = "#ffdd55", 
+  "Thalassiosira" = "#5599ff", 
+  "Minidiscus" = "#515eed",
+  "Synedra" = "#f2556f",
+
+  "Mamiellophyceae" = "#ed0082",
+  "Micromonas" = "#ed0082",
+  "Woloszynskia" = "#eeff00", 
+  "Phaeocystis" = "#c4ff55",
+  "Phaeocystaceae" = "#c4ff55",
+  "Prymnesiophyceae" = "#c4ff55",
+  "Gyrodinium" = "#dbd9b6",
+  "Fragilariales" = "#999999", 
+  "Grammonema" = "#999999", 
+  "Gymnodinium clade" = "#524b00",
+  "Gymnodinium" = "#524b00",
+  "Proboscia" = "#0bc9de",
+  "Suessiaceae" = "#42263f",
+
+  "Dino-I-1" = "#7a81ff", 
+  "Dino-I-1 uc"  = "#ff7a7a",
+  "Dino-II-7" = "#484da8", 
+  "Dino-II-7 uc" = "#a15757", 
+  "RAD-C" = "#afdde9", 
+  "RAD-C uc" = "#afdde9", 
+  "RAD-B" = "#afdde9", 
+  "RAD-B-Group-IV" = "#afdde9",
+  "RAD-B-Group-IV_X" = "#afdde9",
+  "MAST-3" = "#fc60ff",
+  "MAST-1" = "#fc60ff",
+  "Pirsonia_Clade" = "#99832a",
+
+  "Corethron" = "goldenrod",
+  "Tripos" = "#fbdfa2", 
+  "Fragilariopsis" = "#dbd3c8", 
+  "Chytriodinium" = "#afc6e9",
+  "Rhizosolenia" = "#f2556f",
+  "Chromidina" = "#3b6b5d",
+  "Leegaardiella" = "#e6d66e",
+  "Heterocapsa" = "#af82d1",
+  "Odontella" = "#241e23",
+  "Polarella" = "#390959",
+  "Naviculales" = "#996eb5",
+
+  "Chytridiomycetes uc" = "#b900d9",
+  "Labyrinthulaceae uc" = "#762A83")
+
+col.gradient <- c(
+   "Corethron" = "#522178",
+   "Tripos" = "#fbdfa2", 
+   "Fragilariopsis" = "#675478",
+   "Rhizosolenia" ="#efbc82",
+   "Proboscia" ="#9e6374",
+   "Pseudo-nitzschia" = "#de9b71",
+   "RAD-C uc" = "#afdde9",
+   "Ascidiaceihabitans" = "#668000", 
+   "SAR116" = "#b9c06f", 
+   "Luteolibacter" = "#afa20b",
+   "Magnetospiraceae" = "#909090",
+   "Cand_Nitrosopumilus" = "#8ee8ff", 
+   "Arctic97B-4 uc" = "#4c6180",
+   "Gammaproteobacteria uc" = "#c8c4b7", 
+   "Amylibacter" = "#2d4030",
+   "Planktomarina" = "#ffcc00",
+   "Colwellia" = "#30af63", 
+   "Dadabacteriales uc" = "#ab37c8", 
+   "Marinimicrobia_SAR406 uc" = "#88f6e1", 
+   "Flavobacterium" = "#d3ff2e", 
+   "Chaetoceros" = "#ff5ab1",
+   "Pterocystida uc" = "#0066ff",
+   "Bacillaria" ="#4a6125", 
+   "Naviculales" = "#e0d12f",
+   "Polarella"="#b9c06f", 
+   "Chrysophyceae C uc" ="#afa20b",
+   "Syndiniales" = "#0088aa",
+   "icetaxa" = "yellow2")
+               
+col.env <- c(
+  "spring" = "#ffcc00", 
+  "summer" = "#f28500",  
+  "autumn" = "#aa0000",   
+  "winter" = "#50516b",
+  "AW" = "#ee6363", 
+  "PW" = "#009acd", 
+  "mix" = "#B0B0B0",
+  "16S" = "#008080", 
+  "18S" = "#614257",  
+  "WSC" = "indianred2",
+  "EGC" = "deepskyblue3")
+  
+col.heat <- colorRampPalette(c(
+  "#50516b","white","lightyellow","#ffcc00"))(40)
+
